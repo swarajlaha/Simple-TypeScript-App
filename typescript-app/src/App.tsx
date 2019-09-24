c1463a1034759351e9bb3ff34cce49b263b2d9ec
Swaraj Laha (ENU) <swaraj.srj@gmail.com>
2019-09-24 09:04:56 +0530

    Imported CounterOutput and modified

--- typescript-app/src/App.tsx
+++ typescript-app/src/App.tsx
@@ -1,10 +1,9 @@
 import * as React from 'react';
+import CounterOutput from './CounterOutput';
 
 class App extends React.Component {
   public render() {
-    return (
-      
-    );
+    return <CounterOutput counter={1} />;
   }
 }
 
