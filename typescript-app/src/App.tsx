6bdfcebf2d59d78c908e2adcc1aa46174034b7a5
Swaraj Laha (ENU) <swaraj.srj@gmail.com>
2019-09-24 09:31:57 +0530

    Added state and 2 functions

--- typescript-app/src/App.tsx
+++ typescript-app/src/App.tsx
@@ -1,16 +1,34 @@
 import * as React from 'react';
 import CounterOutput from './CounterOutput';
 
-class App extends React.Component {
+interface IAppState {
+  counterValue: number;
+}
+
+class App extends React.Component<{}, IAppState> {
+  public state = {counterValue: 0}
+
   public render() {
     return (
       <div style={{textAlign:'center'}}>
-        <CounterOutput counter={1} />
-        <button>Increment</button>
-        <button>Decrement</button>
+        <CounterOutput counter={this.state.counterValue} />
+        <button onClick={this.incHandler}>Increment</button>
+        <button onClick={this.decHandler}>Decrement</button>
       </div>
     );
   }
+
+  private incHandler = () => {
+    this.setState(prevState => {
+      return {counterValue: prevState.counterValue + 1};
+    });
+  };
+
+  private decHandler = () => {
+    this.setState(prevState => {
+      return {counterValue: prevState.counterValue - 1};
+    });
+  };
 }
 
 export default App;
